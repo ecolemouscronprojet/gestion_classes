e1a52ec70f6ba415175e12bf2aa88259b594a328
samuel lassoie <samuel.lassoie@gmail.com>
2024-02-19 18:57:01 +0100

    wip

--- classes/professeur.php
+++ classes/professeur.php
@@ -1,10 +1,10 @@
 <?php
 
 class Professeur {
-    public int $id;
+    private int $id;
     public string $nom;
     public string $prenom;
     public string $telephone;
-    public Date $date_de_naissance;
+    public DateTime $date_de_naissance;
 
 }
--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
+require_once('classes/professeur.php');
+
+
+$professeur1 = new Professeur();
+$professeur1->id = 1;
+$professeur1->nom = "Lassoie";
+$professeur1->prenom = "Samuel";
+$professeur1->date_de_naissance = new DateTime();
+
+echo '<pre>';
+print_r($professeur1);
+
 
 echo 'HELLLO';
